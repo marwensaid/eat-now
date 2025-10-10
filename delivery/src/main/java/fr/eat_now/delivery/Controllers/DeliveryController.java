1c0afa88b3135370425e81e0968f7968ed023d0f
Mehdi Ghoulam <med@gelk.fr>
2025-10-10 12:45:23 +0200

    feat: add delivery service with basic controller and configuration

---
+++ delivery/src/main/java/fr/eat_now/delivery/Controllers/DeliveryController.java
@@ -0,0 +1,15 @@
+package fr.eat_now.delivery.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/deliveries")
+public class DeliveryController {
+
+    @GetMapping
+    public String getDeliveries() {
+        return "List of deliveries";
+    }
+}
