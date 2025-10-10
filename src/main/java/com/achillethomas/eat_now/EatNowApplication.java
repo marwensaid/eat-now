086af72d58a10b6d656a950506d849994571b44c
Achille POIRIER <achille.poirier01@gmail.com>
2025-10-10 08:51:38 +0200

    Initial commit

---
+++ src/main/java/com/achillethomas/eat_now/EatNowApplication.java
@@ -0,0 +1,13 @@
+package com.achillethomas.eat_now;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/achillethomas/eat_now/EatNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achillethomas.eat_now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
