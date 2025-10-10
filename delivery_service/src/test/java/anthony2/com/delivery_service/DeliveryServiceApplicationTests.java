02f86d8030626b3f264ea490421c7b1f5c70e507
antocreadev <anthonymenghi@icloud.com>
2025-10-10 09:02:16 +0200

    init: 3 microservices

---
+++ delivery_service/src/test/java/anthony2/com/delivery_service/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package anthony2.com.delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
