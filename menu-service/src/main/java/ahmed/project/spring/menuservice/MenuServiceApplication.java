33d6e5c2390239047e00cae6f77de49bfab4cbb4
ahmed <ahmed.dahdouh98@gmail.com>
2025-10-10 09:10:29 +0200

    MenuService first push

---
+++ menu-service/src/main/java/ahmed/project/spring/menuservice/MenuServiceApplication.java
@@ -0,0 +1,13 @@
+package ahmed.project.spring.menuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuServiceApplication.class, args);
+	}
+
+}
---
+++ menu-service/src/test/java/ahmed/project/spring/menuservice/MenuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ahmed.project.spring.menuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
