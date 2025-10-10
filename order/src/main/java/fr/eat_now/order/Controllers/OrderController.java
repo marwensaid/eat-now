0b9f839e2110d36603779241c28e50927ce11c3c
Mehdi Ghoulam <med@gelk.fr>
2025-10-10 12:44:22 +0200

    feat: initialize order service and configuration

---
+++ order/src/main/java/fr/eat_now/order/Controllers/OrderController.java
@@ -0,0 +1,34 @@
+package fr.eat_now.order.Controllers;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    @GetMapping
+    public Map<String, List> all() {
+        return Map.of("data", new ArrayList<>());
+    }
+
+//    @GetMapping()
+//   @Retry(name = "backendRetry", fallbackMethod = "fallbackResponse")
+//    @CircuitBreaker(name = "backendCircuitBreaker", fallbackMethod = "fallbackResponse")
+//    @TimeLimiter(name = "backendTimeout")
+//    public CompletableFuture<String> getData() {
+//
+//    }
+
+}
---
+++ order/src/test/java/fr/eat_now/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eat_now.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
